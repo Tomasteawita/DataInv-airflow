be97f44cbe933b5bf4a03cd245129618d50f504a
Cueva Tomas <B062797@servexternos.santandertecnologia.com.ar>
2024-06-21 17:11:20 -0300

    DataInv1 refactor: Update log table schema
    The log table schema in the init-db.sql file has been updated. New columns `dag_id`, `task_id`, `map_index`, `execution_date`, `run_id`, `owner_display_name` have been added, and the `extra` column has been made nullable. This refactor improves the structure of the log table for better data management.

--- init-db.sql
+++ init-db.sql
@@ -4,9 +4,15 @@ CREATE DATABASE DataInvAirflowDB;
 CREATE TABLE log (
     id SERIAL PRIMARY KEY,
     dttm TIMESTAMP WITH TIME ZONE NOT NULL,
+    dag_id VARCHAR(255),
+    task_id VARCHAR(255),
+    map_index INT,
     event VARCHAR(255) NOT NULL,
+    execution_date TIMESTAMP WITH TIME ZONE,
+    run_id VARCHAR(255),
     owner VARCHAR(255) NOT NULL,
-    extra JSON NOT NULL
+    owner_display_name VARCHAR(255),
+    extra JSON 
 );
 
 -- Crear la tabla 'connection'
@@ -18,7 +24,19 @@ CREATE TABLE connection (
 -- Crear la tabla 'ab_user'
 CREATE TABLE ab_user (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    last_login TIMESTAMP WITH TIME ZONE,
+    login_count INTEGER,
+    fail_login_count INTEGER,
+    created_on TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed_on TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_by_fk INTEGER,
+    changed_by_fk INTEGER
 );
 
 -- Crear la tabla 'ab_register_user'
